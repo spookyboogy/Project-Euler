3d49401dbaaeb3504a9fc59f74c88306ff05c6b0
Matt <matt.tolstoy@gmail.com>
2018-01-24 00:57:04 -0600

    Very small and slow progress.

--- euler.py
+++ euler.py
@@ -1212,7 +1212,7 @@ def thirtyfive():
 def thirtysix():
 
 	"""
-	The decimal number, 585 = 10010010012 (binary), is palindromic in both
+	The decimal number, 585 = 1001001001 (binary), is palindromic in both
 	bases.
 
 	Find the sum of all numbers, less than one million, which are palindromic
@@ -1221,6 +1221,9 @@ def thirtysix():
 	(Please note that the palindromic number, in either base, may not include
 	leading zeros.)
 	"""
+
+	pass
+
 
 
 
